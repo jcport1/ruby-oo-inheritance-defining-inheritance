36d4f2c21d613c2d7af374f7a686c3f510f2a88d
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-11-10 18:34:48 -0500

    testing

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,8 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle #the <means that we inherit the Car class from Vehicle 
+
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end 
+end 
